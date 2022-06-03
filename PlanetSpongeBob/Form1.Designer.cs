f66943c6600a14e2a9d27a2b6ccaafe4811c4f9d
Yunus Emre Çakal <yunusemreckl@hotmail.com>
2022-06-03 23:55:17 +0300

    Add project files.

---
+++ PlanetSpongeBob/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace PlanetSpongeBob
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(42, 145);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.LocationChanged += new System.EventHandler(this.pictureBox1_LocationChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(708, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 15);
+            this.label1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PlateauSpongeBob.Properties.Resources.images;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ PlanetSpongeBob/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
+
+namespace PlanetSpongeBob
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Width = 800;
+            this.Height = 500;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Location = new Point(this.Width - pictureBox1.Width - gridWidth , 0);
+        }
+
+        int gridWidth = 50;
+        int rightCount;
+        int leftCount;
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.R)
+            {
+                rightCount++;
+                Bitmap bitmap = (Bitmap)pictureBox1.Image;
+                pictureBox1.Image = (Image)(RotateImg(bitmap, 90.0f));
+            }
+            if (e.KeyCode == Keys.M)
+            {
+                string direction = FindRotate();
+
+                switch (direction)
+                {
+                    case "E":
+                        if (pictureBox1.Location.X <= this.Width-pictureBox1.Width-gridWidth - 10)
+                        {
+                            pictureBox1.Location = new Point(pictureBox1.Location.X + gridWidth, pictureBox1.Location.Y);
+                        }
+                        else MessageBox.Show("You can't leave away from SpongeBob Plateau");
+                        break;
+                    case "W":
+                        if (pictureBox1.Location.X >= gridWidth )
+                        {
+                            pictureBox1.Location = new Point(pictureBox1.Location.X - gridWidth, pictureBox1.Location.Y);
+                        }
+                        else MessageBox.Show("You can't leave away from SpongeBob Plateau");
+                        break;
+                    case "N":
+                        if (pictureBox1.Location.Y >= gridWidth )
+                        {
+                            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y - gridWidth);
+                        }
+                        else MessageBox.Show("You can't leave away from SpongeBob Plateau");
+                        break;
+                    case "S":
+                        if (pictureBox1.Location.Y <= this.Height-pictureBox1.Height-gridWidth - 10)
+                        {
+                            pictureBox1.Location = new Point(pictureBox1.Location.X, pictureBox1.Location.Y + gridWidth);
+                        }
+                        else MessageBox.Show("You can't leave away from SpongeBob Plateau");
+                        break;
+                }
+            }
+            if (e.KeyCode == Keys.L)
+            {
+                leftCount++;
+                Bitmap bitmap = (Bitmap)pictureBox1.Image;
+                pictureBox1.Image = (Image)(RotateImg(bitmap, -90.0f));
+            }
+        }
+
+        public string FindRotate()
+        {
+            int result = rightCount - leftCount;
+            result = result % 4;
+            switch (result)
+            {
+                case 0: return "N"; break;
+                case 1: return "E"; break;
+                case 2: return "S"; break;
+                case 3: return "W"; break;
+                case -1: return "W"; break;
+                case -2: return "S"; break;
+                case -3: return "E"; break;
+            }
+            return "";
+        }
+
+        public static Bitmap RotateImg(Bitmap bmp, float angle)
+        {
+
+            int w = bmp.Width;
+
+            int h = bmp.Height;
+
+            Bitmap tempImg = new Bitmap(w, h);
+
+            Graphics g = Graphics.FromImage(tempImg);
+
+            g.DrawImageUnscaled(bmp, 1, 1);
+
+            g.Dispose();
+
+            GraphicsPath path = new GraphicsPath();
+
+            path.AddRectangle(new RectangleF(0f, 0f, w, h));
+
+            Matrix mtrx = new Matrix();
+
+            mtrx.Rotate(angle);
+
+            RectangleF rct = path.GetBounds(mtrx);
+
+            Bitmap newImg = new Bitmap(Convert.ToInt32(rct.Width), Convert.ToInt32(rct.Height));
+
+            g = Graphics.FromImage(newImg);
+
+            g.TranslateTransform(-rct.X, -rct.Y);
+
+            g.RotateTransform(angle);
+
+            g.InterpolationMode = InterpolationMode.HighQualityBilinear;
+
+            g.DrawImageUnscaled(tempImg, 0, 0);
+
+            g.Dispose();
+
+            tempImg.Dispose();
+
+            return newImg;
+
+        }
+
+        private void pictureBox1_LocationChanged(object sender, EventArgs e)
+        {
+            label1.Text = pictureBox1.Location.X.ToString() + "," + pictureBox1.Location.Y.ToString() + "," + FindRotate();
+        }
+    }
+}
